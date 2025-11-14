8b55661ce17613658de48496c71456377b8c13db
John Brewer <jbrewer@jera.com>
2025-11-14 11:10:04 -0800

    Initial Commit

---
+++ SurfaceTest/SurfaceTestApp.swift
@@ -0,0 +1,34 @@
+//
+//  SurfaceTestApp.swift
+//  SurfaceTest
+//
+//  Created by John Brewer on 11/14/25.
+//
+
+import SwiftUI
+
+@main
+struct SurfaceTestApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
